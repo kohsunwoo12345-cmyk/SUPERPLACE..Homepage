850e5b94a0a11fe409010070382d5dcdb5e8bba3
kohsunwoo12345-cmyk <genspark_dev@genspark.ai>
2026-01-25 18:18:08 +0000

    Force rebuild: Update timestamp in tuition route handler

--- functions/api/tuition/[[route]].ts
+++ functions/api/tuition/[[route]].ts
@@ -1,7 +1,7 @@
 import tuitionApp from '../../../src/tuition-routes'
 
 export const onRequest: PagesFunction = async (context) => {
-  // Updated: 2026-01-25 - Fixed class_name column references
+  // Updated: 2026-01-25 18:00 - Fixed academy_id references
   console.log('Tuition route called with URL:', context.request.url)
   console.log('Path:', new URL(context.request.url).pathname)
   
